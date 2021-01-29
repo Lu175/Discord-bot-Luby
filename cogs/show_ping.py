620b828ec553ed58c3b595bafa318bf0db6bdb6b
Jaehyun Park <eelu175@gmail.com>
2021-01-30 06:26:58 +0900

    Update show_ping.py
    Change ping test process

--- cogs/show_ping.py
+++ cogs/show_ping.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext import commands
 import Luby_info
-import time
 
 
 class ShowPing(commands.Cog):
@@ -16,33 +15,20 @@ class ShowPing(commands.Cog):
                                    colour=self.Luby_color,
                                    description='**:mailbox_with_mail: Pong !**')
 
-        START = time.time()
-        msg_1 = await ctx.author.send("ping")
-        END = time.time()
-        ping_1 = END-START
-        START = time.time()
-        msg_2 = await ctx.author.send("ping")
-        END = time.time()
-        ping_2 = END-START
-        START = time.time()
-        msg_3 = await ctx.author.send("ping")
-        END = time.time()
-        ping_3 = END-START
+        START = ctx.message.created_at.timestamp()
+        PING_MSG = await ctx.send("ping")
+        END = PING_MSG.created_at.timestamp()
+        PING_TIME = (END-START) % 3600
 
         embed_ping.add_field(name='API Latency',
                              value=f'{round(self.bot.latency * (10 ** 3))} ms',
                              inline=False)
         embed_ping.add_field(name='Latency',
-                             value=f'1: {round(ping_1 * (10 ** 3))} ms\n'
-                                   f'2: {round(ping_2 * (10 ** 3))} ms\n'
-                                   f'3: {round(ping_3 * (10 ** 3))} ms',
+                             value=f'{round(PING_TIME * (10 ** 3))} ms',
                              inline=False)
         embed_ping.set_footer(text=self.Luby_footer)
         await ctx.send(embed=embed_ping)
-        await msg_1.delete()
-        await msg_2.delete()
-        await msg_3.delete()
-        await ctx.author.send("핑~ 핑~ 핑~")
+        await PING_MSG.delete()
 
 
 def setup(bot):
