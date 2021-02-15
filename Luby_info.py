e5ee850211bddf931bb6b7182407d4584b6f6c5d
Lu175 <eelu175@gmail.com>
2021-02-16 03:02:06 +0900

    Update Luby_info.py

--- Luby_info.py
+++ Luby_info.py
@@ -5,7 +5,7 @@ eeLu175_id = 622467096109908008
 Luby_color = 0xAA11CC
 Luby_version = 'v1.0.3-beta'
 Luby_footer = "Luby   | " + Luby_version
-Luby_Activity_name = f"``루비는 현재 {Luby_version} 예요! ㅎㅎ`` ./help  와 여러분의 대화를"
+Luby_Activity_name = f'__루비는 현재 {Luby_version} 예요! ㅎㅎ__ ./help  와 여러분의 대화를'
 
 # current abspath
 Luby_path = ""
