250855205e60a8e2069b9500755773834e693b9e
Lu175 <eelu175@gmail.com>
2021-04-06 07:48:28 +0900

    Revert "Revert "Update v1.0.4-beta""
    This reverts commit 7d2218921ef5f59d5adfa8aaf589f5bd9328c402.

--- Luby_info.py
+++ Luby_info.py
@@ -3,7 +3,7 @@ eeLu175_id = 622467096109908008
 
 # Luby
 Luby_color = 0xAA11CC
-Luby_version = 'v1.0.3-beta'
+Luby_version = 'v1.0.4-beta'
 Luby_footer = "Luby   | " + Luby_version
 Luby_Activity_name = f'__루비는 현재 {Luby_version} 예요! ㅎㅎ__ ./help  와 여러분의 대화를'
 
@@ -18,3 +18,8 @@ BLANK = chr(12644)  # '\u3164'
 BLANK_unicode = chr(65039)  # '\uFE0F'
 B_unicode_Emoji = chr(8205)  # '\u200D'
 
+# Cogs list
+cogs_list = []
+
+# URL
+Luby_thumbnail_url = 'https://lu175.com/pic/swan_1.png'
